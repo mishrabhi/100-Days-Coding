aff532d301b8e0c11b1737fe93e5332b8dbebab4
mishrabhi <asmyselfabhishek00@gmail.com>
2022-11-23 21:13:23 +0530

    added problem 3

---
+++ Python Problems/P3.py
@@ -0,0 +1,5 @@
+#  Get an input character from the user and give the ASCII value of the given input as the output.
+
+ch = input("Enter any character: ")
+print("The ASCII value of char " + ch + " is: ",ord(ch))
+
