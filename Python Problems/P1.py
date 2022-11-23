c33e04082c488d3bdfa33d66e6bcc838481e187b
mishrabhi <asmyselfabhishek00@gmail.com>
2022-11-23 16:03:25 +0530

    first problem

---
+++ Python Problems/P1.py
@@ -0,0 +1,5 @@
+ch = input("Please enter your own character: ")
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+    print("The given character", ch, "is a vowel")
+else:
+    print("The given character", ch, "is a consonant")
