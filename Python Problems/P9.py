fe96a081b9a58ca7f17f29bf83e5983be4e3c491
mishrabhi <asmyselfabhishek00@gmail.com>
2022-11-26 19:20:05 +0530

    added problem 9

---
+++ Python Problems/P9.py
@@ -0,0 +1,13 @@
+#Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
+
+#Input
+
+#3241
+
+#Output
+
+#4
+
+num = int(input("Enter an integer: "))
+String = str(num)
+print(len(String))
