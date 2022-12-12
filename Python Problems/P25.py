5e0209d1039fd96ef962ef87c0a4a82ce940d7b5
mishrabhi <asmyselfabhishek00@gmail.com>
2022-12-12 16:47:10 +0530

    added problem 25

---
+++ Python Problems/P25.py
@@ -0,0 +1,6 @@
+# Get the value for radius from the user and calculate the area of the circle for the given radius.
+
+PI = 3.14
+r = float(input("Enter the radius of circle: "))
+area = PI * r * r
+print("Area of circle = % .2f" %area)
