417bf97cb7d900b6f5f696449da75282818b2998
mishrabhi <asmyselfabhishek00@gmail.com>
2022-12-27 22:54:28 +0530

    added problem 42

---
+++ Python Problems/P42.py
@@ -0,0 +1,12 @@
+# Get two arrays as the input from the user and check whether it is the same or not.
+
+import numpy as np
+x = np.random.randint(0,2,6)
+print("First array:")
+print(x)
+y = np.random.randint(0,2,6)
+print("Second array:")
+print(y)
+print("Test above two arrays are equal or not!")
+array_equal = np.allclose(x, y)
+print(array_equal)
