2a4d5d6b4ec7dd66d7e96c64b892ff914a46cbd2
mishrabhi <asmyselfabhishek00@gmail.com>
2023-01-08 23:10:46 +0530

    added problm 52

---
+++ Python Problems/P52.py
@@ -0,0 +1,10 @@
+# Given an integer array of size N, write a program to reverse the array;
+
+arr = [1, 2, 3, 4, 5];     
+print("Original array: ");    
+for i in range(0, len(arr)):    
+    print(arr[i]),     
+print("Array in reverse order: ");    
+#Loop through the array in reverse order    
+for i in range(len(arr)-1, -1, -1):     
+    print(arr[i]), 
